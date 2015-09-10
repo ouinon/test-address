426ba2ca860b8e28b8f47ad8375b1a8ca6d6949a
Robert Starbuck <robstarbuck@gmail.com>
2015-09-10 19:47:30 +0100

    Completed the name filter, #TODO ensure that when results are removed the filter still works

--- app/scripts/filters/namefilter.js
+++ app/scripts/filters/namefilter.js
@@ -5,12 +5,35 @@ angular.module('addressApp')
 
 	function nameFilter(contacts, search) {
 		
-		if (!search){
+		if (!search.name && !search.phone){
 			// Return everyone
 			return contacts;
 		}
 
-		return $filter('filter')(contacts,{name: search.name, phone: search.phone});
+		return $filter('filter')(contacts,function(contact){
+
+			var result = true;
+			// Probably wouldn't use this syntax ordinarily
+			var search0 = !search.name ? '' : search.name[0].toLowerCase();
+			var name0 = contact.name[0].toLowerCase();
+
+			var searchPhone = !search.phone ? '' : search.phone.replace(/[^0-9]/g,'');
+			var phone = !contact.phone ? '' : contact.phone.replace(/[^0-9]/g,'');
+
+			if(search0 && (search0 !== name0)){
+				result = false;
+			}
+
+			if(searchPhone){
+
+				var regX = new RegExp(searchPhone);
+
+				result = result && regX.test(phone);
+
+			}
+
+			return result;
+		});
 
 	}
 
